b11bbee35348eb2b1bc54539375ae972965c4a44
midyc <164496259+midyc@users.noreply.github.com>
2025-02-05 12:02:00 +0100

    Update push-docker.sh

--- ci/push-docker.sh
+++ ci/push-docker.sh
@@ -2,7 +2,7 @@
 set -e
 Tag='local'
 echo "$docker_password" | docker login ghcr.io --username "$docker_username" --password-stdin
-docker push "ghcr.io/$docker_username/micronaut-app:1.0-${GIT_COMMIT::8}-$Tag" 
+docker push "ghcr.io/$docker_username/micronaut-app:1.0-${GIT_COMMIT::8}$Tag" 
 docker push "ghcr.io/$docker_username/micronaut-app:latest" &
 wait
 
