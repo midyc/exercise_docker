c270ee445014245c5c2ac02b2def0543aa8e6a8f
midyc <164496259+midyc@users.noreply.github.com>
2025-02-05 12:21:20 +0100

    Update build-docker.sh

--- ci/build-docker.sh
+++ ci/build-docker.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -e
-echo '${env.GIT_COMMIT}'
-[[ -z '${env.GIT_COMMIT}' ]] && Tag='local' || Tag='${env.GIT_COMMIT::8}'
+[[ -z ${{ github.sha }} ]] && Tag='local' || Tag=${{ github.sha }::8}
 REPO="ghcr.io/$docker_username/"
 echo "${REPO}"
 docker build -t "${REPO}micronaut-app:latest" -t "${REPO}micronaut-app:1.0-$Tag" app/
