73bb3737cbbcc4d9016f8e8959189c0789ef50c7
midyc <164496259+midyc@users.noreply.github.com>
2025-02-05 13:29:16 +0100

    Update build-docker.sh

--- ci/build-docker.sh
+++ ci/build-docker.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -e
 COMMIT_SHA=$1
-[[ -z "${COMMIT_SHA}" ]] && Tag='local' || Tag="${COMMIT_SHA::8}" 
+echo $COMMIT_SHA
+[[ -z "$COMMIT_SHA" ]] && Tag='local' || Tag="$COMMIT_SHA" 
 REPO="ghcr.io/$docker_username/"
 echo "${REPO}"
 docker build -t "${REPO}micronaut-app:latest" -t "${REPO}micronaut-app:1.0-$Tag" app/
