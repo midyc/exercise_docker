d4695ab2d85c83227769e5bbb4a6e8fde97d3704
midyc <164496259+midyc@users.noreply.github.com>
2025-02-05 12:13:21 +0100

    Update build-docker.sh

--- ci/build-docker.sh
+++ ci/build-docker.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 set -e
+echo ${env.GIT_COMMIT}
+echo '${env.GIT_COMMIT}'
+echo "${env.GIT_COMMIT}"
 [[ -z "${env.GIT_COMMIT}" ]] && Tag='local' || Tag="${env.GIT_COMMIT::8}" 
 REPO="ghcr.io/$docker_username/"
 echo "${REPO}"
